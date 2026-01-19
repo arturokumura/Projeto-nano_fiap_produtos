af46ca94ff9065a1ddf8fddd996cedcb6394b995
arturokumura <arturokumura2008@gmail.com>
2026-01-19 17:25:31 -0300

    Update ProdutoView.java

--- src/view/ProdutoView.java
+++ src/view/ProdutoView.java
@@ -59,6 +59,50 @@ import java.time.LocalDateTime;
                     .setPreco(preco).setDatadeCadastro(LocalDateTime.now());
             return ret;
         }
+
+        public static void update(produto produto) {
+            Categoria categoria = null;
+        do{categoria = CategoriaView.select(produto.getCategoria());
+            produto.setCategoria(categoria);
+        } while (categoria == null);
+
+        String nome = "";
+
+        do {
+            nome = JOptionPane.showInputDialog("Nome do Produto", produto.getNome());
+        if (nome.length() < 3)
+            JOptionPane.showMessageDialog(null, "O nome do porduto precisa ter no mínimo 3 digitos");
+
+        produto.setNome(nome);
+
+        } while (nome.equals(""));
+
+        String descricao = "";
+
+        do {
+            descricao = JOptionPane.showInputDialog("Descrição do Produto", produto.getDescricao());
+        if (descricao.length() < 5)
+            JOptionPane.showMessageDialog(null, "A descrição do porduto precisa ter no mínimo 5 digitos");
+
+        produto.setDescricao(descricao);
+        } while (descricao.equals(""));
+
+        double p = 0;
+        do {
+            try {
+                p = Double.parseDouble(JOptionPane.showInputDialog("Preço do Produto", produto.getPreco()));
+            } catch (NumberFormatException e) {p = 0;}
+            if (p <= 0) JOptionPane.showMessageDialog(null, "Valor inválido");
+
+        } while (p <= 0);
+
+        BigDecimal preco = BigDecimal.valueOf(p);
+        produto.setPreco(preco);
+        sucesso();
+        JOptionPane.showMessageDialog(null, produto.toString());
+        }
+        public static void sucesso() {
+        }
     }
 
 
