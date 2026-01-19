b824305c0d7e23cd2f73e5a5d1da03efbfd5c399
arturokumura <arturokumura2008@gmail.com>
2026-01-19 16:24:27 -0300

    Update CategoriaView.java

--- src/view/CategoriaView.java
+++ src/view/CategoriaView.java
@@ -1,4 +1,34 @@
 package view;
+import model.Categoria;
+import repository.CategoriaCollectionRepository;
+import javax.swing.*;
 
 public class CategoriaView {
-}
+    static CategoriaCollectionRepository repository;
+
+    public static Categoria select(Categoria categoria) {
+
+        Categoria ret = (Categoria) JOptionPane.showInputDialog(
+                null,
+                "Selecione uma categoria",
+                "Menu",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                repository.findAll().toArray(),
+                categoria == null ? 1 : categoria);
+        return ret;
+    }
+        public static Categoria form() {
+            String nome = JOptionPane.showInputDialog("Informe o nome da Categoria");
+            return new Categoria(nome);
+        }
+
+        public void sucesso() {
+            JOptionPane.showMessageDialog(null, "Categoria salva com sucesso!");
+        }
+        public void sucesso(Categoria categoria) {
+            System.out.println(categoria);
+            JOptionPane.showMessageDialog(null, "Categoria " +
+                    categoria.getName().toUpperCase() + " salva com sucesso!");}
+
+    }
